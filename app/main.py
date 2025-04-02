153222e439c87c1d3b95007af5d7255844b689d2
shelna-op <onaparambilshelna@gmail.com>
2025-04-02 19:18:27 +0530

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from app.database import init_db
+from app.routes import books
+from app.routes import borrowing
+from app.routes import borrowing_history
+from app.routes.users import router as users_router
+
+
+app = FastAPI()
+
+init_db()
+
+app.include_router(books.router, prefix="/books", tags=["Books"])
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Library Management System API!"}
+app.include_router(borrowing.router, prefix="/borrowing", tags=["Borrowing"])
+app.include_router(
+    borrowing_history.router, prefix="/borrowing-history", tags=["Borrowing History"]
+)
+app.include_router(users_router)
